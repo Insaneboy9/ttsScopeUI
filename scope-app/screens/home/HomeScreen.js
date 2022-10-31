62dcc748f34f7ca20ea5c3ff0834dcd7db15a23d
PotassiumMaybePermanganate <112390014+PotassiumMaybePermanganate@users.noreply.github.com>
2022-10-31 11:38:18 +0800

    fix the homescreen

--- scope-app/screens/home/HomeScreen.js
+++ scope-app/screens/home/HomeScreen.js
@@ -36,31 +36,37 @@ export default function HomeScreen(props) {
     return new Promise( res => setTimeout(res, delay) );
 }
 
-  async function getScope() {
+  function processScope() {
     var json = {}
-    const snapshot = await firestore().collection("event").get();
-    setScope(snapshot.docs.map(doc => doc.data()))
-    if (scope.length>=0){
-      for (let i=0; i<scope.length; i++){
-        if (json[toDateTime(scope[i]["Wash_Date"])] != null){
-          json[toDateTime(scope[i]["Wash_Date"])].push({name: scope[i].Scope})
-        }else{
-        json[toDateTime(scope[i]["Wash_Date"])] = [{name: scope[i].Scope}]
+    if (scope.length >= 0) {
+      for (let i = 0; i < scope.length; i++) {
+        if (json[toDateTime(scope[i]["Wash_Date"])] != null) {
+          json[toDateTime(scope[i]["Wash_Date"])].push({ name: scope[i].Scope });
+        } else {
+          json[toDateTime(scope[i]["Wash_Date"])] = [{ name: scope[i].Scope }];
+        }
       }
-      }
+      console.log(json)
+      setJsonObj(json)
     }
-    console.log(json)
-    return json
+  }
+  async function getScope() {
+    firestore().collection("event").get().then(snapshot => {
+      setScope(snapshot.docs.map(doc => doc.data()))
+    });
   }
 
   console.log("render")
 
 
   useEffect(() => {
-    getScope().then((json)=> setJsonObj(json))}, [] //need refresh to get data, dk why still
-
+    getScope()}, [] //need refresh to get data, dk why still
     // setJsonObj({"2022-10-28": [{"name": "This_is_positive_RgbsdWfdGdfg"}, {"name": "cluVvyysHJKv3oiUTo5U"}], "2022-11-03": [{"name": "SfbEfdbkJjida"}, {"name": "FewgwDghHdfthdg"}, {"name": "MegEEghEPPP"}], "2022-11-08": [{"name": "FrdfhGhtRhtWtr"}], "2022-11-09": [{"name": "QfbssdvKdsafgwr"}, {"name": "EqetFdsDfdhGfd"}, {"name": "QkbeLcdafnP"}, {"name": "EdssDSGHGdgwDSF"}], "2022-11-10": [{"name": "VngfhgRgrEgn"}, {"name": "WreeFojpBfgGfde"}, {"name": "RsdwwqTiiukJfeg"}, {"name": "DHYasdfgFGFJh"}], "2022-11-11": [{"name": "FberBgfnEfbHwrg"}, {"name": "FuoEilBjhlOrdg"}, {"name": "TthRterhEbhkj"}], "2022-11-12": [{"name": "SfdDgBfjfSHfsfJfCfs"}]})},[]
   )
+  
+  useEffect(() => {
+    processScope()
+  }, [scope])
   
 
   return (
